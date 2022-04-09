8269ead38625b917becf43154c3a2c817466af3a
guilhermesilvafonseca <yguilhermefonseca@gmail.com>
2022-04-08 21:21:58 -0300

    versão 0.0.1 home

--- Site-institucional-react/src/pages/index.jsx
+++ Site-institucional-react/src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../html-css-template/css/style.css'
 import fundo from '../html-css-template/images/fundo.png'
+import logo from '../html-css-template/images/autg.png'
 
 
 function Index() {
@@ -20,10 +21,23 @@ function Index() {
             </nav>
 
             <section id="home">
+
+
+                <div className="texto_inicial">
+                    <img src={logo} alt="" />
+                    <p>
+                        <p className="institucional">Institucional</p>
+                        Empresa focada em soluções tecnologica para o seu negócio  garantindo a economia de energia da sua empresa!
+                    </p>
+                    <button className="btn_incial">Entre em Contato</button>
+
+                </div>
+
                 <img className="img_home" src={fundo} alt="" />
+
             </section>
 
-            
+
         </>
 
     );
