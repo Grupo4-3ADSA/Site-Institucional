f7bf3b7ed4336dd7525de6287ee8d9ebdae00449
Guilherme da Silva Fonseca <guilherme.fonseca@berghem.com.br>
2022-04-15 16:16:48 -0300

    03

--- Site-institucional-react/src/pages/index.jsx
+++ Site-institucional-react/src/pages/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import '../html-css-template/css/style.css'
-import fundo from '../html-css-template/images/fundo.png'
-import logo from '../html-css-template/images/autg.png'
-import beatriz from '../html-css-template/images/beatriz.png'
-import elizeu from '../html-css-template/images/elizeu.png'
-import guilherme from '../html-css-template/images/guilherme.png'
-import jonas from '../html-css-template/images/jonas.png'
-import mateus from '../html-css-template/images/mateus.png'
-import vitoria from '../html-css-template/images/vitoria.png'
-import fundo_sobre from '../html-css-template/images/fundo_sobre.png'
+import '../html-css-template/css/style.css';
+import fundo from '../html-css-template/images/fundo.png';
+import logo from '../html-css-template/images/autg.png';
+import beatriz from '../html-css-template/images/beatriz.png';
+import elizeu from '../html-css-template/images/elizeu.png';
+import guilherme from '../html-css-template/images/guilherme.png';
+import jonas from '../html-css-template/images/jonas.png';
+import mateus from '../html-css-template/images/mateus.png';
+import vitoria from '../html-css-template/images/vitoria.png';
+import fundo_sobre from '../html-css-template/images/fundo_sobre.png';
+import fundo_servicos from '../html-css-template/images/fundo_servicos.png'
+import automacao from '../html-css-template/images/automacao.png'
 
 function Index() {
 
@@ -61,6 +63,23 @@ function Index() {
                     <img src={jonas} alt="" />
                     <img src={mateus} alt="" />
                     <img src={vitoria} alt="" />
+
+                </div>
+
+            </section>
+
+            <section style={{
+                backgroundImage: `url(${fundo_servicos})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 95%'
+
+            }} id="servicos">
+
+                <h1 className="titulo_servicos">Nosso Serviços</h1>
+
+                <div className="nossos_servicos">
+
+                    <img src={automacao} alt="" />
+
+                    <div className="">Automação Eletrica</div>
 
                 </div>
 
